bc419e90156b7ca3ede27d121f5a72a5c0954a85
vrmer <marcell.fekete95@gmail.com>
2021-06-04 17:54:10 +0200

    added an extraction function that can give input to a cue detector

--- scripts/data_exploration.py
+++ scripts/data_exploration.py
@@ -7,45 +7,9 @@ from tqdm import tqdm
 
 import matplotlib.pyplot as plt
 
+import constants
+
 plt.style.use('seaborn')
-
-headers = [
-    'file_name',
-    'sent_num',
-    'token_num_doc',
-    'token_num_sent',
-    'begin_end_offset',
-    'token',
-    'lemma',
-    'pos',
-    'dep_label',
-    'dep_head',
-    'attr_labels'
-]
-
-conll_kwargs = {
-    'delimiter': '\t',
-    'encoding': 'utf8',
-    'names': headers
-}
-
-label_dict = {
-    'sources': [
-        'SOURCE',
-        'B-SOURCE',
-        'I-SOURCE'
-    ],
-    'contents': [
-        'CONTENT',
-        'B-CONTENT',
-        'I-CONTENT'
-    ],
-    'cues': [
-        'CUE',
-        'B-CUE',
-        'I-CUE'
-    ]
-}
 
 
 def extract_label(data, label):
@@ -73,6 +37,7 @@ def extract_label(data, label):
 
 def extract_examples(file_path,
                      target_label='sources',
+                     collect='token',
                      show_errors=False):
     """
     This function takes examples of sources, cues, or content
@@ -80,17 +45,18 @@ def extract_examples(file_path,
 
     :param file_path: input filepath
     :param target_label: sources, contents or cues
+    :param collect:
     :param show_errors: if True, error log is printed containing words that are cues or sources in multiple chains
     :return:
     """
     dataframe = pd.read_csv(
         file_path,
-        **conll_kwargs
+        **constants.conll_kwargs
     )
 
     filtered_dataframe = extract_label(
         dataframe,
-        label_dict[target_label][0]
+        constants.label_dict[target_label][0]
     )
 
     examples = []
@@ -100,32 +66,32 @@ def extract_examples(file_path,
     if filtered_dataframe is not None:
 
         for idx, row in filtered_dataframe.iterrows():
-                labels = row.attr_labels.split()
+            labels = row.attr_labels.split()
 
-                for label in labels:
-                    counter = 0
-                    if label.startswith(label_dict[target_label][1]):
+            for label in labels:
+                counter = 0
+                if label.startswith(constants.label_dict[target_label][1]):
 
-                        if example:
+                    if example:
 
-                            examples.append(
-                                ' '.join(example)
-                            )
+                        examples.append(
+                            ' '.join(example)
+                        )
 
-                            example = []
+                        example = []
 
-                        counter += 1
-                        example.append(row.token)
+                    counter += 1
+                    example.append(row[collect])
 
-                        if counter > 1:
-                            print(f'WARNING: Multiple {label_dict[target_label][1]} in one row!')
-                            errors.append(row.token)
+                    if counter > 1:
+                        print(f'WARNING: Multiple {constants.label_dict[target_label][1]} in one row!')
+                        errors.append(row[collect])
 
-                    elif label.startswith(label_dict[target_label][2]):
-                        example.append(row.token)
+                elif label.startswith(constants.label_dict[target_label][2]):
+                    example.append(row[collect])
 
-                    else:
-                        continue
+                else:
+                    continue
 
     else:
         examples = []
@@ -139,6 +105,9 @@ def extract_examples(file_path,
 all_cues = []
 all_sources = []
 
+all_cue_pos = set()
+all_cue_dep_label = set()
+
 all_files = glob.glob('../data/**/**/**')
 
 non_test_files = [
@@ -147,20 +116,32 @@ non_test_files = [
 ]
 
 # Looping through the files inside all folders that are not part of the test set
-# with tqdm(total=len(non_test_files), desc='Looping through articles: ') as pbar:
-#
-#     for file_path in non_test_files:
-#         # print(file_path)
-#         cue_examples = extract_examples(file_path,
-#                                         target_label='cues')
-#         source_examples = extract_examples(file_path,
-#                                            target_label='sources')
-#
-#         for cue, source in zip(cue_examples, source_examples):
-#             all_cues.append(cue)
-#             all_sources.append(source)
-#
-#         pbar.update(1)
+with tqdm(total=len(non_test_files), desc='Looping through articles: ') as pbar:
+
+    for file_path in non_test_files:
+        # print(file_path)
+        cue_examples = extract_examples(file_path,
+                                        target_label='cues',
+                                        collect='pos')
+        source_examples = extract_examples(file_path,
+                                           target_label='sources',
+                                           collect='dep_label')
+
+        for cue, source in zip(cue_examples, source_examples):
+            all_cue_pos.add(cue)
+            all_cue_dep_label.add(source)
+
+        pbar.update(1)
+
+with open('../data/output/data_exploration/cue_pos_set.txt', 'w') as outfile:
+    for pos in sorted(all_cue_pos):
+        outfile.write(pos)
+        outfile.write('\n')
+
+with open('../data/output/data_exploration/cue_dep_label_set.txt', 'w') as outfile:
+    for dl in sorted(all_cue_dep_label):
+        outfile.write(dl)
+        outfile.write('\n')
 
 
 # counted_cues = Counter(all_cues)
@@ -172,23 +153,23 @@ non_test_files = [
 # with open('counted_sources.pkl', 'wb') as outfile:
 #     pickle.dump(counted_sources, outfile)
 
-with open('counted_cues.pkl', 'rb') as infile:
-    counted_cues = pickle.load(infile)
+# with open('counted_cues.pkl', 'rb') as infile:
+#     counted_cues = pickle.load(infile)
+#
+# with open('counted_sources.pkl', 'rb') as infile:
+#     counted_sources = pickle.load(infile)
 
-with open('counted_sources.pkl', 'rb') as infile:
-    counted_sources = pickle.load(infile)
-
-cue_dict = defaultdict(int)
-source_dict = defaultdict(int)
-
-for cue, freq in counted_cues.items():
-    cue_dict[freq] += 1
-
-for source, freq in counted_sources.items():
-    source_dict[freq] += 1
-
-print(sorted(cue_dict.items(), key=lambda x: x[1], reverse=True))
-print(source_dict)
+# cue_dict = defaultdict(int)
+# source_dict = defaultdict(int)
+#
+# for cue, freq in counted_cues.items():
+#     cue_dict[freq] += 1
+#
+# for source, freq in counted_sources.items():
+#     source_dict[freq] += 1
+#
+# print(sorted(cue_dict.items(), key=lambda x: x[1], reverse=True))
+# print(source_dict)
 
 # cues_df = pd.DataFrame(counted_cues, index=counted_cues.keys())
 # cues_df.drop(cues_df.columns[1:], inplace=True)
