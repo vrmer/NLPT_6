626b4d27b51edf7c2eed04e6fe1dc02745eeeb20
adrielli <adrielli.drica@gmail.com>
2021-06-14 14:22:38 +0200

    fixed sentence getter

--- scripts/baselines.py
+++ scripts/baselines.py
@@ -1,6 +1,7 @@
 import re
 import pandas as pd
 import os
+import networkx as nx
 
 def generate_train_filepaths(path_to_directory, corpus):
     '''
@@ -44,6 +45,7 @@ def read_in_files (path_to_directory, corpus):
                                                     'att'])
         full_df = pd.concat([full_df,data])
 
+
     return full_df
 
 def extract_gold_label(cell):
@@ -75,7 +77,7 @@ class SentenceGetter(object):
                                                                      s["att"].values.tolist(),
                                                                      s["gold"].values.tolist()
                                                                      )]
-        self.grouped = self.data.groupby("sent_n").apply(agg_func)
+        self.grouped = self.data.groupby(["article","sent_n"]).apply(agg_func)
         self.sentences = [s for s in self.grouped]
 
     def get_next(self):
@@ -86,28 +88,104 @@ class SentenceGetter(object):
         except:
             return None
 
+def get_graph (sentence):
+
+    """
+    Given a sentence, create networkx graph with edges corresponding to head-dependent links.
+    :param sentence: sentence object.
+    :return: networkx graph.
+    """
+
+    edges = []  # a  list of tuples with dep links, represented as head and dependent and their respective positions
+    for word in sentence:
+        head_index = word[-3]
+        dep_index = word[1]
+        # dep_label = word[-4]
+        # if dep_label != 'ROOT':
+        #     edges.append((head_index, dep_index))
+        edges.append((head_index, dep_index))
+    graph = nx.Graph(edges)
+
+    return graph
+
 def generate_baseline(sentences, cue_gzt):
 
     predictions = []
     for s in sentences:
-        pred_dict = dict()
+        pred_dict = dict.fromkeys(range(1,len(s)))
+        print(range(len(s)))
+
         # try to find cues by comparing each token lemma with the cue gazetteer
         for token in s:
+
             lemma = token[2]
             if lemma in cue_gzt:
                 idx = token[1]
-                pred_dict[idx] = 'I-CUE'
-                pass
+                pred_dict[idx] = 'CUE'  # if they match, tag token as cue
+                # create networkx graph to more easily access dep structure
+                graph = get_graph(s)
+                print(graph)
+                break
+                # # loop through tokens again to find dependents
+                # for t in s:
+                #
+                #     # if token is dependent and dep label is advmod, neg, aux or mwe, then it is part of the CUE span
+                #     if t[-3] == idx and t[-4] in ["advmod","neg","aux","mwe"]:
+                #         pred_dict[t[1]] = 'CUE'
+                #
+                #     # if token is dependent and dep label is its subject, then it is its SOURCE.
+                #     elif t[-3] == idx and t[-4] == "nsubj":
+                #         pred_dict[t[1]] = 'SOURCE'
+                #
+                #         # its direct and indirect dependents compose the source span.
+                #         for tk in s: # check for first-order dependents
+                #             if tk[-3] == t[1]:
+                #                 pred_dict[tk[1]] = 'SOURCE'
+                #                 for tok in s: # check for second-order dependents
+                #                     if tok[-3] == tk[1]:
+                #                         pred_dict[tok[1]] = 'SOURCE'
+                #
+                #     # if token is dependent and dep label is its clausal complement, then it is its CONTENT.
+                #     elif t[-3] == idx and t[-4] == "ccomp":
+                #         pred_dict[t[1]] = 'CONTENT'
+                #         # its direct and indirect dependents compose the content span
+                #         for tk in s: # check for first-order dependents
+                #             if tk[-3] == t[1]:
+                #                 pred_dict[tk[1]] = 'CONTENT'
+                #                 for tok in s: # check for second-order dependents
+                #                     if tok[-3] == tk[1]:
+                #                         pred_dict[tok[1]] = 'CONTENT'
+
                 predictions.append(pred_dict)
+        print(predictions)
+        break
     return predictions
 
 
+# generate full training data frame
 # df = read_in_files('../../data_ar', 'parc')
 # df["gold"] = df["att"].apply(extract_gold_label) # strip underscores and unwanted labels from attribution column
 # df.to_csv('../data/full_train_dataset_parc.tsv',sep='\t')
+
+# read in full training data frame
 df = pd.read_csv('../data/full_train_dataset_parc.tsv',sep='\t')
+
+# check most frequent dep_labels for each gold label to support syntactic baseline dev
+# with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+#     print(df.groupby("gold")["dep_label"].value_counts(normalize=True))
+
+# create sentence instances
 getter = SentenceGetter(df)
 sentences = getter.sentences
+
+for t in sentences[0]:
+    print(t)
+
+# collect gold labels
 gold = [[token[-1] for token in sentence] for sentence in getter.sentences]
+
+# read in list of reporting verbs from literature
 cue_gzt = pd.read_csv('../data/cue_list.csv')["cue"].tolist()
+
+# generate syntactic baseline
 pred = generate_baseline(sentences, cue_gzt)
